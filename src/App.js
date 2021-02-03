3ab8e3b288fe08dba6a3613afec7614ca74f1fca
Celestin Niyindagiriye <cniyindagiriye@gmail.com>
2021-02-03 12:07:17 +0200

    Initialize project using Create React App
    ch(setup project): project structure
    \- add tailwindcss
    \- configure eslint
    [Done]

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles/button.module.css';
+
+function App() {
+  return (
+    <div className={styles.button}>
+      <h1 className="mt-4 text-white">I am comming</h1>
+    </div>
+  );
+}
+
+export default App;
